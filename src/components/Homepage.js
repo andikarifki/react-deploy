212d9a21a4001f4c3d268b5f78b3ad7a0831ce6a
andika rifki <andikarifki03@gmail.com>
2024-11-14 14:34:31 +0700

    add cv

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -4,6 +4,7 @@ import "./css/Profile.css";
 import profilePicture from "../assets/images/profiles.jpg"; // Naik satu level ke src
 import Icon from "./Icon";
 import "./css/About.css";
+import { FaTelegram } from "react-icons/fa";
 
 function Homepage() {
   return (
@@ -14,13 +15,23 @@ function Homepage() {
       <div className="about">
         <h1 className="heading-name">Andika Rifqi Istanto</h1>
         <h2 className="heading-position">Frontend Developer</h2>
-        <a
-          href="https://andikarifki.github.io/react-deploy/CV.pdf" // Pastikan file PDF berada di folder public
-          download
-          className="link-pdf"
-        >
-          Download CV
-        </a>
+        <div className="link-container">
+          <a
+            href="https://andikarifki.github.io/react-deploy/CV.pdf"
+            download
+            className="link-pdf"
+          >
+            Download CV
+          </a>
+          <a
+            href="https://t.me/Andikarifki18" // Ganti dengan username Telegram Anda
+            target="_blank"
+            rel="noopener noreferrer"
+            className="link-telegram"
+          >
+            Contact Me <FaTelegram size={28} style={{ marginLeft: "5px" }} />
+          </a>
+        </div>
       </div>
       <div className="profile">
         <img src={profilePicture} alt="Profile" className="profile-picture" />
