111cd1d97a133c0fcbcab3aab833d562d6fa4438
andika rifki <andikarifki03@gmail.com>
2024-11-14 09:14:41 +0700

    add cv

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -16,8 +16,8 @@ function Homepage() {
         <h2 className="heading-position">Frontend Developer</h2>
         <section className="section-link">
           <a
-            href="/cv-1.pdf" // Path menuju file CV di folder public
-            download // Menyatakan bahwa file ini adalah untuk diunduh
+            href={`${process.env.PUBLIC_URL}/cv-1.pdf`}
+            download
             className="link-pdf"
           >
             Download CV
