ce72ea1d7ae18297aa3bc5f255826e9818b5a496
andika rifki <andikarifki03@gmail.com>
2024-11-14 09:46:48 +0700

    add cv

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -14,15 +14,13 @@ function Homepage() {
       <div className="about">
         <h1 className="heading-name">Andika Rifqi Istanto</h1>
         <h2 className="heading-position">Frontend Developer</h2>
-        <section className="section-link">
-          <a
-            href="/cv-1.pdf" // Path menuju file CV di folder public
-            download // Menyatakan bahwa file ini adalah untuk diunduh
-            className="link-pdf"
-          >
-            Download CV
-          </a>
-        </section>
+        <a
+          href="/cv-1.pdf" // Path menuju file CV di folder public
+          download // Menyatakan bahwa file ini adalah untuk diunduh
+          className="link-pdf"
+        >
+          Download CV
+        </a>
       </div>
       <div className="profile">
         <img src={profilePicture} alt="Profile" className="profile-picture" />
