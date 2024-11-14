abd514506cb093c85418d2859f5da58964287d7f
andika rifki <andikarifki03@gmail.com>
2024-11-14 23:13:29 +0700

    update

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -2,6 +2,7 @@ import React from "react";
 import About from "./About";
 import Profile from "./Profile";
 import Header from "./Header";
+import "./css/Homepage.css"; // Mengimpor CSS dari folder yang sama
 
 function Homepage() {
   return (
